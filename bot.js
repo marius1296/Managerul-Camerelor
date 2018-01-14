e80d656740ced14413c0040288b2351dc695148c
marius1296 <35421796+marius1296@users.noreply.github.com>
2018-01-14 14:25:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,14 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 // Connexion to Discord API
-bot.login('NDAyMDUyODExOTc1NTU3MTIx.DTzT1w.rF7BTPE5lCS15tXOisC82wFp0Ko');
-    
-// Removes every game roles when the bot logs in
+bot.login('NDAyMDUyODExOTc1NTU3MTIx.DTzIYg.pkrhXpPA_rq5PBdgm65Lf7Zgi-c');
+
+// Bot connexion
+bot.on("ready", function () {
+
+	console.log("The bot is online !");
+
+    // Removes every game roles when the bot logs in
     bot.guilds.forEach(function(guild) { // For each guild
         guild.members.forEach(function(member) { // For each membre
             member.roles.forEach(function(role) { // For each role
@@ -14,6 +19,12 @@ bot.login('NDAyMDUyODExOTc1NTU3MTIx.DTzT1w.rF7BTPE5lCS15tXOisC82wFp0Ko');
     });
 
 });
+
+// Bot disconnection
+bot.on("disconnect", function () {
+	console.log("Disconnected...");
+});
+
 
 // Feature : The bot gives members a role depending the game they are currently playing
 bot.on("presenceUpdate", (oldMember, newMember) => {
@@ -33,9 +44,9 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
         if (gamename.startsWith("Counter-Strike")) { // EX : Specific to Counter-Strike : Global Offensive, because it's too long to create a role with this name
             newMember.addRole(guild.roles.find("name","ðŸŽ® Counter-Strike"))
                 .then(function() {
-                   
+                    console.log(`Role : Counter-Strike given to ${newMember.user.username}`);
                 },function(e) {
-                    
+                    console.error(e);
                 });
         }
         else { // If a role "ðŸŽ® [name]" exists, gives it
@@ -43,9 +54,9 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
             if (newrole) {
                 newMember.addRole(guild.roles.find("name",`ðŸŽ® ${gamename}`))
                     .then(function() {
-                        
+                        console.log(`Role : ${gamename} given to ${newMember.user.username}`);
                     },function(e) {
-                        
+                        console.error(e);
                     });
             }
         }
@@ -55,18 +66,18 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 		if (gamename.startsWith("DOTA")) { // EX : Specific to Counter-Strike : Global Offensive, because it's too long to create a role with this name
             newMember.addRole(guild.roles.find("name","ðŸŽ® Dota2"))
                 .then(function() {
-                    
+                    console.log(`Role : Dota2 given to ${newMember.user.username}`);
                 },function(e) {
-                    
+                    console.error(e);
                 });
         }
 		////////////////////////////////  GTA
 		if (gamename.startsWith("Grand")) { // EX : Specific to Counter-Strike : Global Offensive, because it's too long to create a role with this name
             newMember.addRole(guild.roles.find("name","ðŸŽ® GTA"))
                 .then(function() {
-                    
+                    console.log(`Role : GTA given to ${newMember.user.username}`);
                 },function(e) {
-                    
+                    console.error(e);
                 });
         }
 		
@@ -74,9 +85,9 @@ bot.on("presenceUpdate", (oldMember, newMember) => {
 		if (gamename.startsWith("Metin" || "METIN")) { // EX : Specific to Counter-Strike : Global Offensive, because it's too long to create a role with this name
             newMember.addRole(guild.roles.find("name","ðŸŽ® Metin2"))
                 .then(function() {
-                  
+                    console.log(`Role : Metin2 given to ${newMember.user.username}`);
                 },function(e) {
-                    
+                    console.error(e);
                 });
         }
 		
